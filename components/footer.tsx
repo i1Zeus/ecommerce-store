5559467b2b7b7ee63d264b28ef442b6f088f8e1e
Hussein Najah <husseinnajah123@gmail.com>
2023-07-16 20:52:05 +0300

    nav + footer

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto p-y-10 ">
+        <p className="text-center text-sm text-black">
+          &copy; 2023 Sirbot, Inc All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
