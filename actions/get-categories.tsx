dfa35e706dee04eed74ccaa5fe2dd81a4f655ff5
Hussein Najah <husseinnajah123@gmail.com>
2023-07-17 01:26:16 +0300

    get

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -1,6 +1,6 @@
 import { Category } from "@/types";
 
-const URL = `http://localhost:3000/api/4b415ec1-297d-4d9e-b87e-bfaf2ae713cd/categories`;
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 const getCategories = async (): Promise<Category[]> => {
   const res = await fetch(URL);
