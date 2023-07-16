121d5606a387cd9dc99c4fda3a27fa6574127456
Hussein Najah <husseinnajah123@gmail.com>
2023-07-17 02:53:56 +0300

    get + show + add billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,20 @@
-const HomePage = () => {
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("32933864-a38b-4707-9f78-3fcadf6f5caf");
+
   return (
-    <div>
-      <div>Home page</div>
-    </div>
+    <>
+      <Container>
+        <div className="space-y-10 pb-10">
+          <Billboard data={billboard} />
+        </div>
+      </Container>
+    </>
   );
 };
 
