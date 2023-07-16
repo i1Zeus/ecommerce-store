6ec6f1abc9b3a22b474332190cd5693ece244fb5
Hussein Najah <husseinnajah123@gmail.com>
2023-07-17 02:56:53 +0300

    types

--- types.ts
+++ types.ts
@@ -9,3 +9,32 @@ export interface Category {
   name: string;
   billboard: Billboard;
 }
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  size: Size;
+  color: Color;
+  images: Image[];
+  isFeatured: boolean;
+  isArchived: boolean;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
