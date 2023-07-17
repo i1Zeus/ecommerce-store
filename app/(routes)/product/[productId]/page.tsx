f0fb44ef6ab709481ee1982e1b25ddf76f3cc161
Hussein Najah <husseinnajah123@gmail.com>
2023-07-17 16:12:23 +0300

    gallery

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import getProduct from "@/actions/get-product";
 import getProducts from "@/actions/get-products";
+import Gallery from "@/components/gallery";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
@@ -23,7 +24,7 @@ const ProductPage: React.FC<ProductPageProps> = async ({ params }) => {
       <Container>
         <div className="px-4 py-1 sm:px-6 lg:px-8">
           <div className="lg:grid  lg:grid-cols-2 lg:items-start lg:gap-8">
-            {/* <Gallery images={product.images} /> */}
+            <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               {/* Info */}
               Info
