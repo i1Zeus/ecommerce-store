8b5521046871e52d5ee9681ba39cfbb4fd910e0a
Hussein Najah <husseinnajah123@gmail.com>
2023-07-17 18:53:31 +0300

    filter

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,4 +1,15 @@
+import Container from "@/components/ui/container";
+import Billboard from "@/components/ui/billboard";
+import ProductCard from "@/components/ui/product-card";
+import NoResults from "@/components/ui/no-results";
+
 import getProducts from "@/actions/get-products";
+import getCategory from "@/actions/get-category";
+import getSizes from "@/actions/get-sizes";
+import getColors from "@/actions/get-colors";
+
+import Filter from "./components/filter";
+// import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -6,7 +17,7 @@ interface CategoryPageProps {
   params: {
     categoryId: string;
   };
-  searchParma: {
+  searchParams: {
     colorId: string;
     sizeId: string;
   };
@@ -14,17 +25,41 @@ interface CategoryPageProps {
 
 const CategoryPage: React.FC<CategoryPageProps> = async ({
   params,
-  searchParma,
+  searchParams,
 }) => {
   const products = await getProducts({
     categoryId: params.categoryId,
-    colorId: searchParma.colorId,
-    sizeId: searchParma.sizeId,
+    colorId: searchParams.colorId,
+    sizeId: searchParams.sizeId,
   });
+  const sizes = await getSizes();
+  const colors = await getColors();
+  const category = await getCategory(params.categoryId);
 
-  
-
-  return <div></div>;
+  return (
+    <div className="bg-white">
+      <Container>
+        <Billboard data={category.billboard} />
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            {/* <MobileFilters sizes={sizes} colors={colors} /> */}
+            <div className="">
+              <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default CategoryPage;
