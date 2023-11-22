04eee6c133d2c0052047e37f45095a07685975f7
Hussein Najah <husseinnajah123@gmail.com>
2023-11-22 17:51:50 +0300

    formatted

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -40,16 +40,16 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
     <div className="bg-white">
       <Container>
         <Billboard data={category.billboard} />
-        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+        <div className="sm:px-6 lg:px-8 px-4 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
             <MobileFilters sizes={sizes} colors={colors} />
-            <div className="hidden lg:block">
+            <div className="lg:block hidden">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
             </div>
-            <div className="mt-6 lg:col-span-4 lg:mt-0">
+            <div className="lg:col-span-4 lg:mt-0 mt-6">
               {products.length === 0 && <NoResults />}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              <div className="sm:grid-cols-2 md:grid-cols-3 grid grid-cols-1 gap-4">
                 {products.map((item) => (
                   <ProductCard key={item.id} data={item} />
                 ))}
